af89ec3350223bfce5b531e08baa95c6cff23fb9
godov_gr <godov@git.com>
2023-11-03 01:29:36 +0300

    increase result  lisd and add ligfile

--- start.py
+++ start.py
@@ -1,10 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
+import logging
+
+# Настройка логирования
+logging.basicConfig(filename='google_dork_search.log', level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', filemode='w')
 
 # Функция для выполнения поиска с использованием Google Dork
 def google_dork_search(query):
-
-    url = f'https://www.google.com/search?q=filetype:pdf+{query}'
+    
+    url = f'https://www.google.com/search?q=filetype:pdf+{query}&num=50'
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
     }
@@ -16,19 +20,26 @@ def google_dork_search(query):
 
         # Обработка результатов
         search_results = soup.find_all('div', class_='tF2Cxc')
+        if not search_results:
+            logging.info("Нет результатов по запросу.")
+            return
+
         for result in search_results:
             title = result.h3.text
             link = result.a['href']
             print(f'Title: {title}')
+            logging.info(f'Title: {title}')
             print(f'Link: {link}')
+            logging.info(f'Link: {link}')
             print('----------------------------------------------------------------------------------------')
+            logging.info('----------------------------------------------------------------------------------------')
 
     except requests.exceptions.RequestException as e:
-        print(f'Произошла ошибка при выполнении запроса: {e}')
+        logging.error(f'Произошла ошибка при выполнении запроса: {e}')
 
     except Exception as e:
-        print(f'Произошла ошибка: {e}')
+        logging.error(f'Произошла ошибка: {e}')
 
 if __name__ == "__main__":
-    user_query = input("Введите ключ-слово для поиска: ")
+    user_query = input("Введите ключевое слово для поиска: ")
     google_dork_search(user_query)
